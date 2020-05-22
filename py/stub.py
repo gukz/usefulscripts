ce6790c2987fd3c4665c4042f7141e1623eb63c8
gukz <gukz@qq.com>
2020-05-22 06:42:49 +0000

    Update bash

--- py/stub.py
+++ py/stub.py
@@ -1,10 +1,10 @@
 import grpc
-from protos.octopus.octopus_pb2_grpc import OctopusStub
-from protos.octopus import octopus_pb2
+from protos.octopus_pb2_grpc import OctopusStub
+from protos import octopus_pb2
 
 
 class Stub:
-    def __init__(self, host, port, auth, stub_cls):
+    def __init__(self, host, port, stub_cls):
         channel = grpc.insecure_channel("{}:{}".format(host, port),)
         self.stub = stub_cls(channel)
 
@@ -13,9 +13,9 @@ class Stub:
 
 
 if __name__ == "__main__":
-    stub = Stub("127.0.0.1", "10003", "bayuser-rpc.xyz", OctopusStub)
+    stub = Stub("127.0.0.1", "10003", OctopusStub)
     #  req = octopus_pb2.CreateServiceReq(service_name="postman-rpc.xyz")
     #  res = stub.CreateService(req)
-    #  req = octopus_pb2.GetServiceReq(service_name="postman-rpc.xyz")
-    #  res = stub.GetService(req)
+    req = octopus_pb2.GetServiceReq(service_name="codetime-rpc.codetime")
+    res = stub.GetService(req)
     print(res)
