5f0b9a06544cb952c65fcd33b08074a3465dce35
gukz <gukz@qq.com>
2020-05-26 08:11:15 +0000

    Read from closed chan

--- go/chan.go
+++ go/chan.go
@@ -29,11 +29,35 @@ func commonRecover() {
 	dupCloseChan()
 }
 
+func alwaysReadFromChan() {
+	ch := make(chan int, 5)
+	vals := []int{1, 2, 3, 4, 5}
+	for _, v := range vals {
+		ch <- v
+	}
+	close(ch)
+	c := 0
+	for {
+		select {
+		case v, ok := <-ch:
+			fmt.Println(v, ok)
+			if !ok {
+				goto END
+			}
+		default:
+			c += 1
+		}
+		if c > 10 {
+			break
+		}
+	}
+END:
+	fmt.Println(c)
+}
+
 func main() {
 	commonRecover()
-	for {
-		time.Sleep(1 * time.Second)
-		fmt.Println("main loop")
-
-	}
+	time.Sleep(1 * time.Second)
+	fmt.Println("main loop")
+	alwaysReadFromChan()
 }
