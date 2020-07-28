714b77748acd695f437d6a7c37b34e6d90ada028
gukz <gukz@qq.com>
2020-07-28 11:57:17 +0000

    Allow install dotnet runtime

--- bash/build_csharp_dev.sh
+++ bash/build_csharp_dev.sh
@@ -1,6 +1,30 @@
-sudo apt install gnupg ca-certificates
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
-sudo apt update
+install_mono() {
+    sudo apt install gnupg ca-certificates
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+    echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+    sudo apt update
 
-sudo apt install mono-devel
+    sudo apt install mono-devel
+}
+
+install_dotnet() {
+    wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+    sudo dpkg -i packages-microsoft-prod.deb
+    sudo apt-get update; \
+    sudo apt-get install -y apt-transport-https && \
+    sudo apt-get update && \
+    sudo apt-get install -y dotnet-sdk-3.1
+}
+
+option=$1
+shift
+case "$option" in 
+    install_dotnet)
+        install_dotnet
+        ;;
+    install_mono)
+        install_mono
+        ;;
+esac
+
+exit 0
