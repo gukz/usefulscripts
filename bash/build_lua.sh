ac6eb3c838a21aab8a38786296102b4de9bc686f
gukz <gukz@qq.com>
2020-08-04 23:10:57 +0800

    Remove some files

--- bash/build_lua.sh
+++
@@ -1,5 +0,0 @@
-curl -R -O http://www.lua.org/ftp/lua-5.3.0.tar.gz
-tar zxf lua-5.3.0.tar.gz
-cd lua-5.3.0
-make linux test
-make install
--- bash/config_vim.sh
+++ bash/config_vim.sh
@@ -3,10 +3,10 @@ config_vim() {
     test -d ~/.oh-my-zsh || git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
     test -d ~/.oh-my-zsh/plugins/zsh-autosuggestions || git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
     test -d ~/.oh-my-zsh/plugins/zsh-syntax-highlighting || git clone git@github.com:zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
-    ln -s /g/vim_config ~/.vim
-    ln -s /g/vim_config/vimrc ~/.vimrc
-    ln -s /g/vim_config/zshrc ~/.zshrc
-    ln -s /g/vim_config/tmux.conf ~/.tmux.conf
+    ln -s /mnt/c/dev/vim_config ~/.vim
+    ln -s /mnt/c/dev/vim_config/vimrc ~/.vimrc
+    ln -s /mnt/c/dev/vim_config/zshrc ~/.zshrc
+    ln -s /mnt/c/dev/vim_config/tmux.conf ~/.tmux.conf
     chsh -s /bin/zsh
 }
 config_vim
@@ -25,3 +25,5 @@ config_git() {
 }
 
 config_git
+
+compaudit | xargs chmod g-w,o-w
