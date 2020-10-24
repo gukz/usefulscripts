2469303e59cf67382637dcdd35c52dda009e639f
Gang Wang <wanggang@microsoft.com>
2020-10-24 14:02:34 +0800

    Add script to get all metro station

--- bash/config_vim.sh
+++ bash/config_vim.sh
@@ -84,6 +84,8 @@ pip3 install -U flake8
 sudo apt autoclean
 
 config_vim() {
+    test -d ~/.zinit ||mkdir ~/.zinit
+    git clone https://github.com/zdharma/zinit.git ~/.zinit/bin
     test -d ~/.oh-my-zsh || git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
     test -d ~/.oh-my-zsh/plugins/zsh-autosuggestions || git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
     test -d ~/.oh-my-zsh/plugins/zsh-syntax-highlighting || git clone git@github.com:zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
